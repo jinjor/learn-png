349d93042ebb5244e9fe0d182f7bffca71094cbe
Yosuke Torii <jinjorweb@gmail.com>
2024-02-03 22:12:30 +0900

    refactor test

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import { parse } from "./sync";
 import { requestPixelStream } from "./stream";
+import { Chunk } from "./parse";
 
 (async () => {
   const files = fs.readdirSync("assets");
@@ -10,44 +11,57 @@ import { requestPixelStream } from "./stream";
     {
       const start = Date.now();
       const data = fs.readFileSync(filePath);
-      const { pixels, compressedDataSize, uncompressedDataSize } = await parse(
-        data.buffer
-      );
+      const { chunks, pixels, compressedDataSize, uncompressedDataSize } =
+        await parse(data.buffer);
       let i = 0;
       for (const row of pixels) {
         i++;
       }
+      const end = Date.now();
+      let idatCount = 0;
+      for (const chunk of chunks) {
+        if (chunk.type === "IDAT") {
+          idatCount++;
+          process.stdout.write(`\r  ${chunk.type} x${idatCount}`);
+        } else {
+          if (idatCount > 0) {
+            console.log();
+          }
+          console.log(`  ${chunk.type}`);
+        }
+      }
       console.log(
         `  data size: ${compressedDataSize} -> ${uncompressedDataSize}`
       );
-      console.log(`  sync: ${Date.now() - start}ms`);
+      console.log(`  sync: ${end - start}ms`);
     }
-    if (file.includes("interlace")) {
-      continue;
-    }
-    {
-      const start = Date.now();
-      const stream = fs.createReadStream(filePath);
-      let i = 0;
-      const { head, body } = await requestPixelStream(stream);
-      for await (const row of body) {
-        i++;
-      }
-      console.log(`  stream: ${Date.now() - start}ms`);
-    }
-    {
-      const start = Date.now();
-      const stream = fs.createReadStream(filePath);
-      let i = 0;
-      const { head, body } = await requestPixelStream(stream);
-      for await (const row of body) {
-        i++;
-        if (i === 1) {
-          break;
+    if (!file.includes("interlace")) {
+      {
+        const start = Date.now();
+        const stream = fs.createReadStream(filePath);
+        let i = 0;
+        const { head, body } = await requestPixelStream(stream);
+        for await (const row of body) {
+          i++;
         }
+        console.log(`  stream: ${Date.now() - start}ms`);
       }
-      console.log(`  stream|1px: ${Date.now() - start}ms`);
+      {
+        const start = Date.now();
+        const stream = fs.createReadStream(filePath);
+        let i = 0;
+        const { head, body } = await requestPixelStream(stream);
+        for await (const row of body) {
+          i++;
+          if (i === 1) {
+            break;
+          }
+        }
+        console.log(`  stream|1px: ${Date.now() - start}ms`);
+      }
     }
+
+    console.log();
   }
 })().catch((e) => {
   console.log(e);
