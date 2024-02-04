9730610c02a93ddbaab5d070020f9885c4442cf4
Yosuke Torii <jinjorweb@gmail.com>
2024-02-04 21:39:18 +0900

    introduce vite

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
-import { parse } from "./sync";
-import { requestPixelStream } from "./stream";
+import { parse } from "./lib/sync";
+import { requestPixelStream } from "./lib/stream";
 
 (async () => {
   const files = fs.readdirSync("assets");
