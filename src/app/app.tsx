7ef6d678d09541f47f9905d3f2c63c6b9eb1d2a4
Yosuke Torii <jinjorweb@gmail.com>
2024-02-06 01:24:33 +0900

    style viewer

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { parse } from "../lib/sync";
 import { requestPixelStream } from "../lib/stream";
 
@@ -25,40 +25,98 @@ export const App = () => {
 };
 
 const Item = ({ imagePath }: { imagePath: string }) => {
-  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const canvasContainerRef = React.useRef<HTMLDivElement>(null);
+  const [log, setLog] = React.useState<string[]>([]);
+
+  const reset = () => {
+    setLog([]);
+    const container = canvasContainerRef.current!;
+    container.innerHTML = "";
+    return container;
+  };
+
+  const appendLog = useCallback((s: string) => {
+    setLog((prev) => [...prev, s]);
+  }, []);
 
   const handleClickRead = async () => {
-    const canvas = canvasRef.current!;
+    const container = reset();
     const src = imagePath + "?" + Date.now();
     const start = Date.now();
-    await fetchAndDrawSync(src, canvas);
+    await fetchAndDrawSync(src, container, appendLog);
     console.log("time1:", Date.now() - start);
   };
+
   const handleClickReadStream = async () => {
-    const canvas = canvasRef.current!;
+    const container = reset();
     const src = imagePath + "?" + Date.now();
     const start = Date.now();
-    await fetchAndDrawStream(src, canvas);
+    await fetchAndDrawStream(src, container, appendLog);
     console.log("time2:", Date.now() - start);
   };
+
   return (
-    <>
-      <div style={{ display: "flex", gap: 10, width: "100%" }}>
-        <img src={imagePath} style={{ width: "0%", flexGrow: 1 }} />
-        <canvas ref={canvasRef} style={{ width: "0%", flexGrow: 1 }}></canvas>
+    <div style={{ display: "flex", gap: 30 }}>
+      <div style={{ width: 400, flexShrink: 0 }}>
+        <img src={imagePath} style={{ width: "100%" }} />
+        <ul
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+            marginTop: 20,
+          }}
+        >
+          <li>
+            <button onClick={handleClickRead}>Read (sync)</button>
+          </li>
+          <li>
+            <button onClick={handleClickReadStream}>Read (stream)</button>
+          </li>
+        </ul>
       </div>
-      <button onClick={handleClickRead}>Read</button>
-      <button onClick={handleClickReadStream}>Read (stream)</button>
-    </>
+      <div style={{ flexGrow: 1 }}>
+        <div style={{ position: "relative", marginBottom: 20 }}>
+          <img
+            src={imagePath}
+            style={{ width: "100%", visibility: "hidden" }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              width: "100%",
+              backgroundColor: "#eee",
+            }}
+            ref={canvasContainerRef}
+          ></div>
+        </div>
+        <pre>{log.length ? log.join("\n") : "(No log)"}</pre>
+      </div>
+    </div>
   );
 };
 
-const fetchAndDrawSync = async (src: string, canvas: HTMLCanvasElement) => {
+const createNewCanvas = (container: HTMLDivElement) => {
+  const canvas = document.createElement("canvas");
+  canvas.style.width = "100%";
+  container.appendChild(canvas);
+  return canvas;
+};
+
+const fetchAndDrawSync = async (
+  src: string,
+  container: HTMLDivElement,
+  log: (s: string) => void
+) => {
+  log("started");
   const res = await fetch(src);
   const binary = await res.arrayBuffer();
   const { pixels } = await parse(binary);
   const width = pixels[0].length;
   const height = pixels.length;
+  const canvas = createNewCanvas(container);
   canvas.width = width;
   canvas.height = height;
   const ctx = canvas.getContext("2d")!;
@@ -71,11 +129,18 @@ const fetchAndDrawSync = async (src: string, canvas: HTMLCanvasElement) => {
       ctx.fillRect(x, y, 1, 1);
     }
   }
+  log("done");
 };
 
-const fetchAndDrawStream = async (src: string, canvas: HTMLCanvasElement) => {
+const fetchAndDrawStream = async (
+  src: string,
+  container: HTMLDivElement,
+  log: (s: string) => void
+) => {
+  log("started");
   const res = await fetch(src);
   const { head, body } = await requestPixelStream(bufferStream(res.body!));
+  const canvas = createNewCanvas(container);
   canvas.width = head.width;
   canvas.height = head.height;
   const ctx = canvas.getContext("2d")!;
@@ -85,6 +150,7 @@ const fetchAndDrawStream = async (src: string, canvas: HTMLCanvasElement) => {
       ctx.fillRect(x, y, interpolation?.spanX ?? 1, interpolation?.spanY ?? 1);
     }
   }
+  log("done");
 };
 async function* bufferStream(stream: ReadableStream<Uint8Array>) {
   // const reader = new ReadableStream<Uint8Array>(
